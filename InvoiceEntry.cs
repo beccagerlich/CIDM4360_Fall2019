4ebd07432bd69e4fbec88d06399f1e69a3e366a1
Becca Gerlich <becca_gerlich@android-20c0cab99563ad99.localdomain>
2019-11-04 17:45:03 -0600

    updated code

---
+++ InvoiceEntry.cs
@@ -0,0 +1,12 @@
+//Becca Gerlich, Gregory Sprock, & Cedric Whitaker
+
+using System;
+using System.Collections.Generic;
+
+namespace Homework_4
+{
+    class InvoiceEntry
+    {
+        
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,37 @@
+//Becca Gerlich, Gregory Sprock, & Cedric Whitaker
+
+using System;
+using System.Collections.Generic;
+
+namespace Homework_4
+{
+    class Item
+    {
+        int ID;
+        float UnitPrice;
+        string Description;
+        int AvailableQty;
+
+        public void getItemDescription()
+        {
+
+        }
+        public void getPrice()
+        {
+
+        }
+        public void getItemID()
+        {
+
+        }
+        public void updtAvlblQnty()
+        {
+
+        }
+        public void getAvlblQnty()
+        {
+
+        }
+        
+    }
+}
